189e6bd964926d2e51509c7e8c18299c2a1155ad
Omaima Boustik <omaimaboustik@gmail.com>
2026-02-18 17:57:33 +0100

    Fix assets paths for GitHub Pages

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,14 @@
 module.exports = function (eleventyConfig) {
-
   eleventyConfig.addPassthroughCopy("assets");
   eleventyConfig.addPassthroughCopy("sitemap.xml");
   eleventyConfig.addPassthroughCopy("robots.txt");
 
   return {
+    pathPrefix: "/tamimtsouss/",
     dir: {
       input: ".",
       includes: "_includes",
-      output: "docs"   // ✅ بدل _site
+      output: "docs"
     }
   };
 };
